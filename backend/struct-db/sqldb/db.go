a9e4aafa926817d5d0703f37873775095d600dba
Dempsey Palacio Tascon <dempsey.2001@hotmail.co.uk>
2024-04-09 15:14:48 +0100

    changes

--- backend/struct-db/sqldb/db.go
+++ backend/struct-db/sqldb/db.go
@@ -3,6 +3,8 @@ package sqldb
 import (
 	"database/sql"
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/go-sql-driver/mysql"
 
@@ -14,22 +16,21 @@ func ConnectToDB() *sql.DB {
 	if err := godotenv.Load(); err != nil {
 		fmt.Println("No .env file found")
 	}
-	/*
-		var (
-			host, _        = os.LookupEnv("HOST")
-			port_string, _ = os.LookupEnv("PORT")
-			port, err      = strconv.Atoi(port_string)
-			admin, _       = os.LookupEnv("ADMIN")
-			password, _    = os.LookupEnv("PASSWORD")
-			dbname, _      = os.LookupEnv("DBNAME")
-		)*/
+	var (
+		host, _        = os.LookupEnv("HOST")
+		port_string, _ = os.LookupEnv("PORT")
+		port, err      = strconv.Atoi(port_string)
+		admin, _       = os.LookupEnv("ADMIN")
+		password, _    = os.LookupEnv("PASSWORD")
+		dbname, _      = os.LookupEnv("DBNAME")
+	)
 
 	cfg := mysql.Config{
-		User:   "root",
-		Passwd: "Amanchooses1@",
+		User:   admin,
+		Passwd: password,
 		Net:    "tcp",
-		Addr:   "127.0.0.1:3306",
-		DBName: "userauth",
+		Addr:   fmt.Sprintf("%s:%d", host, port),
+		DBName: dbname,
 	}
 	db, err := sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
